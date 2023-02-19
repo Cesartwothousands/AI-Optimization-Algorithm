b07f6570d0b703f645fb6f9f706fc1b18b900e1a
XUQIPAN <1668112439@qq.com>
2023-02-19 13:29:11 -0500

    finish Problem 8. Search algorithms.

--- N Queens and A satr search/mp520.py
+++ N Queens and A satr search/mp520.py
@@ -1,4 +1,5 @@
 import random
+import heapq
 
 ''' ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ '''
 '''
@@ -9,90 +10,158 @@ import random
 '''
 BFS add to queue 
 '''
+
+BFS_queue = []
+DFS_stack = []
+priority_queue = []
+
+
+class Node:
+    def __init__(self, node_id, parent_node_id, cost):
+        self.node_id = node_id
+        self.parent_node_id = parent_node_id
+        self.cost = cost
+
+    def __lt__(self, other):
+        return self.cost < other.cost
+
+
 def add_to_queue_BFS(node_id, parent_node_id, cost, initialize=False):
     # Your code here
-     return
+    if initialize:
+        BFS_queue.clear()
+    BFS_queue.append((node_id, parent_node_id))
+    return
+
 
 '''
 BFS add to queue 
 '''
+
+
 def is_queue_empty_BFS():
     # Your code here
-    return False
+    return len(BFS_queue) == 0
+
 
 '''
 BFS pop from queue
 '''
+
+
 def pop_front_BFS():
     (node_id, parent_node_id) = (0, 0)
     # Your code here
-    return (node_id, parent_node_id)
+    (node_id, parent_node_id) = BFS_queue.pop(0)
+    return node_id, parent_node_id
+
 
 '''
 DFS add to queue 
 '''
+
+
 def add_to_queue_DFS(node_id, parent_node_id, cost, initialize=False):
     # Your code here
+    if initialize:
+        DFS_stack.clear()
+    DFS_stack.append((node_id, parent_node_id))
     return
+
 
 '''
 DFS add to queue 
 '''
+
+
 def is_queue_empty_DFS():
     # Your code here
-    return False
+    return len(DFS_stack) == 0
+
 
 '''
 DFS pop from queue
 '''
+
+
 def pop_front_DFS():
     (node_id, parent_node_id) = (0, 0)
     # Your code here
-    return (node_id, parent_node_id)
+    (node_id, parent_node_id) = DFS_stack.pop()
+    return node_id, parent_node_id
+
 
 '''
 UC add to queue 
 '''
+
+
 def add_to_queue_UC(node_id, parent_node_id, cost, initialize=False):
     # Your code here
+    if initialize:
+        priority_queue.clear()
+    heapq.heappush(priority_queue, Node(node_id, parent_node_id, cost))
     return
+
 
 '''
 UC add to queue 
 '''
+
+
 def is_queue_empty_UC():
     # Your code here
-    return False
+    return len(priority_queue) == 0
+
 
 '''
 UC pop from queue
 '''
+
+
 def pop_front_UC():
     (node_id, parent_node_id) = (0, 0)
     # Your code here
-    return (node_id, parent_node_id)
+    node = heapq.heappop(priority_queue)
+    (node_id, parent_node_id) = node.node_id, node.parent_node_id
+    return node_id, parent_node_id
+
 
 '''
 A* add to queue 
 '''
+
+
 def add_to_queue_ASTAR(node_id, parent_node_id, cost, initialize=False):
     # Your code here
+    if initialize:
+        priority_queue.clear()
+    heapq.heappush(priority_queue, Node(node_id, parent_node_id, cost))
     return
+
 
 '''
 A* add to queue 
 '''
+
+
 def is_queue_empty_ASTAR():
     # Your code here
-    return False
+    return len(priority_queue) == 0
+
 
 '''
 A* pop from queue
 '''
+
+
 def pop_front_ASTAR():
     (node_id, parent_node_id) = (0, 0)
     # Your code here
-    return (node_id, parent_node_id)
+    node = heapq.heappop(priority_queue)
+    (node_id, parent_node_id) = node.node_id, node.parent_node_id
+    return node_id, parent_node_id
+
 
 ''' ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ '''
 '''
@@ -100,41 +169,45 @@ def pop_front_ASTAR():
 '''
 ''' ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ '''
 
-
 '''
 Compute a random state 
 '''
+
+
 def get_random_state(n):
     state = []
-    # Your code here 
+    # Your code here
     return state
+
 
 '''
 Compute pairs of queens in conflict 
 '''
+
+
 def compute_attacking_pairs(state):
     number_attacking_pairs = 0
-    # Your code here 
+    # Your code here
     return number_attacking_pairs
+
 
 '''
 The basic hill-climing algorithm for n queens
 '''
+
+
 def hill_desending_n_queens(state, comp_att_pairs):
     final_state = []
     # Your code here
     return final_state
 
+
 '''
-Hill-climing algorithm for n queens with restart
+Hill-climbing algorithm for n queens with restart
 '''
+
+
 def n_queens(n, get_rand_st, comp_att_pairs, hill_descending):
     final_state = []
     # Your code here
     return final_state
-
-
-
-
-
-
